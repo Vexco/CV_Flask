82e64450e02e8209f007ef38f8c879ad87ccf0f9
Xavier MANTELLATO <xavier.mantellato@arkance-systems.com>
2024-02-20 16:35:17 +0100

    updates

--- __init__.py
+++ __init__.py
@@ -45,8 +45,6 @@ def ajouter_message():
         email = request.form['email']
         message = request.form['msg']
         
-        return {email, message}
-
         # Insérer les données dans la base de données (ici, je suppose que tu as une table 'clients')
         conn = sqlite3.connect('database.db')
         cursor = conn.cursor()
