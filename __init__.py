06f808836ccd6a9e37de01b03853c817f282fb91
Xavier MANTELLATO <xavier.mantellato@arkance-systems.com>
2024-02-27 11:04:43 +0100

    update init db

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from urllib.request import urlopen
 import sqlite3
 
 app = Flask(__name__) #creating flask app name
-db = "sqlite:///database.db"
+db = "database.db"
 
 def get_db_connection():
     connection = sqlite3.connect(db)
